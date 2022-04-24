3c30bc302e38859ec23fc12fda9bd022c79c89b7
Alexander Smirnov <ru.alexander.smirnov@gmail.com>
2022-04-24 17:18:26 +0300

    use borrow

--- P09_AaveBrownie/scripts/use_aave.py
+++ P09_AaveBrownie/scripts/use_aave.py
@@ -1,7 +1,8 @@
 from scripts.utils import get_account
 from scripts.aave import Aave
 
-LEND_AMOUNT = 0.01 * 1e18
+# LEND_AMOUNT = 0.01 * 1e18
+LEND_AMOUNT = 10 * 1e18
 
 def main():
     account = get_account()
@@ -14,6 +15,10 @@ def main():
     aave.deposit_eth(LEND_AMOUNT)
     aave.print_state()
 
-    # Withdraw
-    aave.withdraw_aave_weth_to_eth()
+    # # Borrow
+    aave.borrow(5 * 1e18)
     aave.print_state()
+
+#     # Withdraw
+#     aave.withdraw_aave_weth_to_eth()
+#     aave.print_state()
