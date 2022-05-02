8d6ad278e8dc829abb92dd65ee2e3182f5b85686
Alexander Smirnov <ru.alexander.smirnov@gmail.com>
2022-05-02 23:01:53 +0300

    add full cycle testing

--- P09_AaveBrownie/scripts/use_aave.py
+++ P09_AaveBrownie/scripts/use_aave.py
@@ -1,10 +1,8 @@
 from scripts.utils import get_account
 from scripts.aave import Aave
 
-LEND_AMOUNT = 0.1 * 1e18
-# LEND_AMOUNT = 100 * 1e18
-TOKEN_TO_BORROW = "btc"
-# TOKEN_TO_BORROW = "dai"
+DEPOSIT_AMOUNT = 0.01 * 1e18
+TOKEN_TO_BORROW = "dai"  # one of ("dai", "btc")
 
 def main():
     account = get_account()
@@ -14,13 +12,17 @@ def main():
     aave.print_state()
 
     # Deposit
-    aave.deposit_eth(LEND_AMOUNT)
+    aave.deposit_eth(DEPOSIT_AMOUNT)
     aave.print_state()
 
     # Borrow
     aave.borrow()
     aave.print_state()
 
-#     # Withdraw
-#     aave.withdraw_aave_weth_to_eth()
-#     aave.print_state()
+    # Repay
+    aave.repay()
+    aave.print_state()
+
+    # Withdraw
+    aave.withdraw_aave_weth_to_eth()
+    aave.print_state()
